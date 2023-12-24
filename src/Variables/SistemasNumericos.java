a0eecf2ed8001ebc4debbcedddb6b924e08c63ba
Ian <115850010+NaiZenay@users.noreply.github.com>
2023-12-24 16:26:18 -0600

    Sistemas numericos utilizado Scanner

--- src/Variables/SistemasNumericos.java
+++ src/Variables/SistemasNumericos.java
@@ -1,18 +1,31 @@
 package Variables;
 import javax.swing.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class SistemasNumericos {
     public static void main(String[] args) {
-      String inputNumero= JOptionPane.showInputDialog(null,"Ingreesa un numero Decimal");
-      int numeroDecimal=Integer.parseInt(inputNumero);
-      String resultado=
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese cualquier numero entero");
+        int numeroDecimal=0;
+        try{
+            //Lee la entrada de datos numerica en la terminal
+            numeroDecimal= scanner.nextInt();
+
+            //Excepcion en caso de que el dato ingresado no sea un numero
+        }catch (InputMismatchException ime){
+            System.out.println("Numero invalido");
+            //Finaliza programa
+            System.exit(0);
+        }
+        String resultado=
 //Metodo para convertir un numero decimal a Binario
                 "Numero Binario de "+numeroDecimal+"="+Integer.toBinaryString(numeroDecimal)+"\n"+
 //Metodo para convertir un numero decimal a Octal
                 "Numero Octal de "+numeroDecimal+"="+Integer.toOctalString(numeroDecimal)+"\n"+
 //Metodo para convertir un numero decimal a Hexadecimal
                 "Numero Hexadecimal de "+numeroDecimal+"="+Integer.toHexString(numeroDecimal);
-
-      JOptionPane.showMessageDialog(null,resultado);
+        System.out.println(resultado);
     }
 
     //Declaracion de sistemas numericos en enteros

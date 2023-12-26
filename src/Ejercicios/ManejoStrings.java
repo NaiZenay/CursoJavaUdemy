154ae1abc5b957a2ecde22a0bdd4e58eee543dd8
Ian <115850010+NaiZenay@users.noreply.github.com>
2023-12-25 23:33:57 -0600

    Ejercicio de manjeo de strings

--- src/Ejercicios/ManejoStrings.java
+++ src/Ejercicios/ManejoStrings.java
@@ -1,2 +1,18 @@
-package Ejercicios;public class ManejoStrings {
+package Ejercicios;
+
+import javax.swing.*;
+import java.util.Locale;
+
+public class ManejoStrings {
+    public static void main(String[] args) {
+        String[] nombres = new String[10];
+        String[] nombresConvertidos = new String[10];
+        String resultado = "";
+        for (int i = 0; i < 3; i++) {
+            nombres[i] = JOptionPane.showInputDialog(null, "Ingresa el nombre de algun familiar o amigo");
+            nombresConvertidos[i] = String.valueOf(nombres[i].toUpperCase().charAt(1)).concat(".").concat(nombres[i].substring(nombres[i].length() - 2)).concat("_");
+            resultado += nombresConvertidos[i];
+        }
+        JOptionPane.showMessageDialog(null, resultado);
+    }
 }

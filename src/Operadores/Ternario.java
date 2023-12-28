17f8baab524b4412f5806fcbcaa336fd0c995513
Ian <115850010+NaiZenay@users.noreply.github.com>
2023-12-27 19:01:36 -0600

    Operadores ternarios y un ejemplo

--- src/Operadores/Ternario.java
+++ src/Operadores/Ternario.java
@@ -1,12 +1,31 @@
 package Operadores;
 
+import java.util.Scanner;
+
 public class Ternario {
     public static void main(String[] args) {
-        int i =10;
-        int j =20;
-        boolean b1= i<j;
-        System.out.println(b1?"aprobado":"reprobado");
-                        //if(condicion)?, true : false
+        int i = 10;
+        int j = 20;
+        boolean b1 = i < j;
+        System.out.println(b1 ? "aprobado" : "reprobado");
+        //if(condicion)?, true : false
+
+        Scanner scan = new Scanner(System.in);
+        int num1;
+        int num2;
+        int num3;
+        int max;
+        System.out.println("Ingresa un numero entero");
+        num1 = scan.nextInt();
+        System.out.println("Ingresa un numero entero");
+        num2 = scan.nextInt();
+        System.out.println("Ingresa un numero entero");
+        num3 = scan.nextInt();
+        max=(num1 > num2)?num1:num2;
+        max=(max>num3)?max:num3;
+
+        System.out.println("El numero mas grande de los tres es:" + max);
+
 
         //Jerarquia de operaciones
         /*

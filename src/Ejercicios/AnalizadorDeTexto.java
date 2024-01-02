ffeb50fde8375188360d299569fc2fa2d2555f09
Ian <115850010+NaiZenay@users.noreply.github.com>
2024-01-02 16:10:31 -0600

    Refactor de codigo y areglo de bugs

--- src/Ejercicios/AnalizadorDeTexto.java
+++ src/Ejercicios/AnalizadorDeTexto.java
@@ -10,52 +10,61 @@ public class AnalizadorDeTexto {
         System.out.println("Ingrese el texto a analizar");
         String inputTexto = scanner.nextLine();
 
-        String[] letras = new String[3];
+        //Cuantas letras se van a buscar
+        String[] letras = new String[2];
         int[] countLetras = new int[letras.length];
 
-        System.out.println("Ingrese 3 caracteres a analizar dentro del texto");
-        //Pedir letras sl usuario
+        System.out.println("Ingrese " + letras.length + " caracteres a analizar dentro del texto");
+        //Pedir letras al usuario
+
+        boolean charInvalido;
+        //if there is a god is fking me!!!
         for (int i = 0; i < letras.length; i++) {
-            letras[i] = scanner.nextLine();
+            System.out.println("Ingresa caracter");
+            do {
+                charInvalido = true;
+                letras[i] = scanner.nextLine();
+                if (letras[i].length() > 1) {
+                    System.out.println("Ingresaste mas de un caracter, Solo debes ingresar 1");
+                    charInvalido = false;
+                }
+            } while (!charInvalido);
         }
 
         //Llenar contadores
+        //if there is a god is fking me!!!
         for (int i = 0; i < inputTexto.toCharArray().length; i++) {
-            if (String.valueOf(inputTexto.toCharArray()[i]).equalsIgnoreCase(letras[0])) {
-                countLetras[0]++;
-            } else if (String.valueOf(inputTexto.toCharArray()[i]).equalsIgnoreCase(letras[1])) {
-                countLetras[1]++;
-            } else if (String.valueOf(inputTexto.toCharArray()[i]).equalsIgnoreCase(letras[2])) {
-                countLetras[2]++;
+            for (int j = 0; j < letras.length; j++) {
+                if (String.valueOf(inputTexto.toCharArray()[i]).equalsIgnoreCase(letras[j])) {
+                    countLetras[j]++;
+                }
             }
         }
-
+        StringBuilder instanciasLetras = new StringBuilder();
         for (int i = 0; i < countLetras.length; i++) {
-            System.out.println("Hay " + countLetras[i] + " instancias de la letra " + letras[i]);
+            instanciasLetras.append("Hay ").append(countLetras[i]).append(" instancias de la letra ").append(letras[i]).append("\n");
         }
+        System.out.println(instanciasLetras);
 
-        System.out.println();
 
         //Contar palabras
-        System.out.println("Hay " + inputTexto.split(" ").length + " palabras en este texto");
-
-        System.out.println();
+        System.out.println("Hay " + inputTexto.split(" ").length + " palabras en este texto\n");
 
         //Primer y ultimo caracter
-        System.out.println("La letra \"" + inputTexto.toCharArray()[0] + "\" es el primer caracter del texto y \"" + inputTexto.toCharArray()[inputTexto.toCharArray().length - 1] + "\" es el ultimo caracter del texto");
-
-        System.out.println();
+        System.out.println("La letra \"" + inputTexto.toCharArray()[0] + "\" es el primer caracter del texto y \"" + inputTexto.toCharArray()[inputTexto.toCharArray().length - 1] + "\" es el ultimo caracter del texto \n");
 
         //Texto Invertido
-        System.out.println("Como se veria el texto invertido?");
-        for (int i=inputTexto.split(" ").length-1 ; i>0 ;i--){
-            System.out.print(inputTexto.split(" ")[i]+ " ");
+        int maxTexto = inputTexto.split(" ").length;
+        StringBuilder textoInvertido = new StringBuilder();
+        textoInvertido.append("Como se veria el texto con las palabras invertidas?").append("\n");
+        while (maxTexto > 0) {
+            textoInvertido.append(inputTexto.split(" ")[maxTexto - 1]).append(" ");
+            maxTexto--;
         }
-
-        System.out.println();
+        System.out.println(textoInvertido + "\n");
 
         //Verficar si la palabra java se encuentra dentro de este texto
-        if (inputTexto.contains("Java")){
+        if (inputTexto.contains("Java")) {
             System.out.println("La palabra \"Java\" esta dentro de este texto");
         }
     }

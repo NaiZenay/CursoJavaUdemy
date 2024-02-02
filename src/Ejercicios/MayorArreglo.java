6572ec9490794bce848e3eac46007db03d8bb9ce
Ian <115850010+NaiZenay@users.noreply.github.com>
2024-02-01 20:07:05 -0600

    obtener el valor mas alto de un array

--- src/Ejercicios/MayorArreglo.java
+++ src/Ejercicios/MayorArreglo.java
@@ -1,2 +1,22 @@
-package Ejercicios;public class MayorArreglo {
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class MayorArreglo {
+    public static void main(String[] args) {
+        int[] nums=new int[5];
+        int mayor=0;
+        Scanner scanner= new Scanner(System.in);
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scanner.nextInt();
+
+        }
+        for (int i = 1; i < nums.length; i++) {
+            mayor=(nums[mayor]>nums[i])?mayor:i;
+
+        }
+
+        System.out.println(nums[mayor]);
+
+    }
 }

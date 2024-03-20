c0538965d2db544a5bb2382035002704f038f75f
Ian <115850010+NaiZenay@users.noreply.github.com>
2024-03-19 22:38:08 -0600

    mostrar detalle de factura y calcular total

--- src/Ejercicios/appFacturas/Factura.java
+++ src/Ejercicios/appFacturas/Factura.java
@@ -1,25 +1,26 @@
 package Ejercicios.appFacturas;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Factura {
     private Cliente cliente;
     private ItemFactura itemsFactura[];
-    private int indexItemFactura=0;
+    private int indexItemFactura = 0;
     private int folio;
     private String descripcion;
     private Date fecha;
 
-    private static final int MAX_ITEMS=10;
+    private static final int MAX_ITEMS = 10;
 
-    private static int ultimoFolio=0;
+    private static int ultimoFolio = 0;
 
     public Factura(Cliente cliente, String descripcion) {
         this.cliente = cliente;
         this.descripcion = descripcion;
-        this.itemsFactura=new ItemFactura[MAX_ITEMS];
-        this.folio=++ultimoFolio;
-        this.fecha=new Date();
+        this.itemsFactura = new ItemFactura[MAX_ITEMS];
+        this.folio = ++ultimoFolio;
+        this.fecha = new Date();
     }
 
     public Cliente getCliente() {
@@ -35,9 +36,9 @@ public class Factura {
     }
 
     public void addItemsFactura(ItemFactura itemFactura) {
-        if(!(indexItemFactura>=MAX_ITEMS)){
+        if (!(indexItemFactura >= MAX_ITEMS)) {
             this.itemsFactura[indexItemFactura++] = itemFactura;
-        }else {
+        } else {
             System.out.println("Se alcanzo el limite de items por factura");
         }
     }
@@ -60,5 +61,49 @@ public class Factura {
 
     public void setFecha(Date fecha) {
         this.fecha = fecha;
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (ItemFactura item : this.itemsFactura) {
+            if (item != null) {
+                total += item.calcularImporte();
+            }
+        }
+        return total;
+    }
+
+    public String mostrarDetalle() {
+        StringBuilder sb = new StringBuilder("Factura No.");
+        sb.append(folio)
+                .append("\nCliente:")
+                .append(this.getCliente().getNombre())
+                .append("\t nif: ")
+                .append(this.getCliente().getNif())
+                .append("\nDescripcion:")
+                .append(this.getDescripcion()).append("\n")
+                .append("\n#\tNombre\t$\tCantidad\tTotal\n");
+
+        for (ItemFactura item : this.itemsFactura) {
+            if (item != null) {
+                sb.append(item.getProducto().getCodigo())
+                        .append("\t")
+                        .append(item.getProducto().getNombre())
+                        .append("\t")
+                        .append(item.getProducto().getPrecio())
+                        .append("\t")
+                        .append(item.getCantidad())
+                        .append("\t")
+                        .append(item.calcularImporte())
+                        .append("\n");
+            }
+        }
+        sb.append("Total: $")
+                .append(calcularTotal());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MMMM-dd");
+        sb.append("Fecha de Facturazion:\n");
+        sb.append(sdf.format(this.getFecha()));
+
+        return sb.toString();
     }
 }

becba881005e0f4edb87bacb9ed6d74f0b90d0c7
Ian <115850010+NaiZenay@users.noreply.github.com>
2024-03-26 18:10:23 -0600

    Control de tipos

--- src/POO/Herencia/EjemploHerencia.java
+++ src/POO/Herencia/EjemploHerencia.java
@@ -5,15 +5,16 @@ public class EjemploHerencia {
         Alumno alumno = new Alumno("ColegioX");
         alumno.setNombre("Julian");
         alumno.setApellido("Luna");
-        alumno.email="julian.luna123@gmail.com";//al ser protected el atributo es accesible por clases hijas sin problemas
+        alumno.email = "julian.luna123@gmail.com";//al ser protected el atributo es accesible por clases hijas sin problemas
         alumno.setCalificacionMatematicas(9.8);
         alumno.setCalificacionFisica(10.0);
         alumno.setCalificacionHistoria(6.8);
+        imprimir(alumno);
 
-        AlumnoInternacional alumnoInternacional= new AlumnoInternacional("Alemania");
+        AlumnoInternacional alumnoInternacional = new AlumnoInternacional("Alemania");
         alumnoInternacional.setNombre("Hanz");
         alumnoInternacional.setApellido("Wolf");
-        alumnoInternacional.email="hanz.wolf765@gmail.com";
+        alumnoInternacional.email = "hanz.wolf765@gmail.com";
         alumnoInternacional.setCalificacionFisica(8.8);
         alumnoInternacional.setCalificacionHistoria(10);
         alumnoInternacional.setCalificacionMatematicas(7.6);
@@ -23,15 +24,38 @@ public class EjemploHerencia {
         Profesor profesor = new Profesor();
         profesor.setNombre("Ricardo");
         profesor.setApellido("Lopez");
-        profesor.email="rochard.lopez098@gmail.com";
+        profesor.email = "rochard.lopez098@gmail.com";
         profesor.setAsignatura(Asignaturas.MATEMATICAS);
 
-        Persona profesor2=new Profesor();
+        Persona profesor2 = new Profesor();
         //Solo puede acceder a los metodos de Persona
         profesor2.setNombre("Brayan");
         profesor2.setEdad(30);
         //Cast de tipo profesor para poder acceder al metodo de la clase
-        ((Profesor)profesor2).setAsignatura(Asignaturas.HISTORIA);
+        ((Profesor) profesor2).setAsignatura(Asignaturas.HISTORIA);
 
+    }
+
+    public static void imprimir(Persona persona) {
+        System.out.println("nombre: " + persona.getNombre() + "\n" +
+                "apellido: " + persona.getApellido() + "\n" +
+                "edad: " + persona.getEdad() + "\n" +
+                "email: " + persona.email);
+
+        if (persona instanceof Alumno) {
+            System.out.println("Colegio: " + (((Alumno) persona).getColegio()) + "\n" +
+                    "promedio:" + (((Alumno) persona).getPromedio()) + "\n" +
+                    "calificacion de Historia:" + (((Alumno) persona).getCalificacionHistoria()) + "\n" +
+                    "calificacion de Matematicas:" + (((Alumno) persona).getCalificacionMatematicas()) + "\n" +
+                    "calificacion de Fisica:" + (((Alumno) persona).getCalificacionFisica()) + "\n"
+            );
+            if (persona instanceof AlumnoInternacional) {
+                System.out.println("pais: " + ((AlumnoInternacional) persona).getPais() + "\n" +
+                        "calificacion de idiomas: " + ((AlumnoInternacional) persona).getNotaIdiomas());
+            }
+        } else if (persona instanceof Profesor) {
+            System.out.println("asignatura: " + ((Profesor) persona).getAsignatura());
+
+        }
     }
 }

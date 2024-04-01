94f70b1061af02cdfc83b2e7d05793d1e82a9944
Ian <115850010+NaiZenay@users.noreply.github.com>
2024-04-01 00:06:25 -0600

    Ejecutable con lo objetos

--- src/Ejercicios/Catalogo/ProyectoCatalogo.java
+++ src/Ejercicios/Catalogo/ProyectoCatalogo.java
@@ -1,2 +1,32 @@
-package Ejercicios.Catalogo;public class ProyectoCatalogo {
+package Ejercicios.Catalogo;
+
+import Ejercicios.Catalogo.Productos.Electronicos.IPhone;
+import Ejercicios.Catalogo.Productos.Electronicos.TVLcd;
+import Ejercicios.Catalogo.Productos.Libros.Comics;
+import Ejercicios.Catalogo.Productos.Libros.Libro;
+import Ejercicios.Catalogo.Productos.Producto;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class ProyectoCatalogo {
+    public static void main(String[] args) throws ParseException {
+        IProducto[] productos= new Producto[5];
+        Calendar calendar= Calendar.getInstance();
+        calendar.set(2020, 0, 01);
+        Date fecha = calendar.getTime();
+
+        productos[0]=new IPhone(30000,"China","14 PRO MAX","Gris");
+        productos[1]=new TVLcd(90000,"Korea",200);
+        productos[2]=new Libro(1000, fecha,"Martin","GG","Editorial de Alemania del Este");
+        productos[3]=new Comics(20,fecha,"Stan Lee","Spiderman ContraAtq","Marvel STudios","Spiderman");
+        productos[4]=new IPhone(19000,"Singapour","SE","ROJO");
+
+        for (IProducto producto:productos){
+            System.out.println(producto);
+        }
+
+    }
 }

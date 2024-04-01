960c81aed53268043de7d39be7f17e442a73604b
Ian <115850010+NaiZenay@users.noreply.github.com>
2024-04-01 00:07:33 -0600

    Error con la herencia

--- src/Ejercicios/Catalogo/Productos/Libros/Comics.java
+++ src/Ejercicios/Catalogo/Productos/Libros/Comics.java
@@ -1,12 +1,26 @@
 package Ejercicios.Catalogo.Productos.Libros;
 
-import java.util.ListResourceBundle;
+import java.util.Date;
 
 public class Comics extends Libro {
     private String personaje;
 
+    public Comics(int precio, Date fechaPublicacion, String autor, String titulo, String editorial, String personaje) {
+        super(precio, fechaPublicacion, autor, titulo, editorial);
+        this.personaje = personaje;
+    }
+
+    public String getPersonaje() {
+        return personaje;
+    }
+
     @Override
     public double getPrecioVenta() {
-        return super.getPrecioVenta();
+        return super.getPrecioVenta()+20;
+    }
+
+    @Override
+    public String toString() {
+        return "El comic del famoso personaje "+personaje+" "+super.toString();
     }
 }
--- src/Ejercicios/Catalogo/Productos/Libros/Libro.java
+++ src/Ejercicios/Catalogo/Productos/Libros/Libro.java
@@ -10,6 +10,14 @@ public class Libro extends Producto implements ILibro{
     private String titulo;
     private String editorial;
 
+    public Libro(int precio, Date fechaPublicacion, String autor, String titulo, String editorial) {
+        super(precio);
+        this.fechaPublicacion = fechaPublicacion;
+        this.autor = autor;
+        this.titulo = titulo;
+        this.editorial = editorial;
+    }
+
     @Override
     public Date getFechaPublicacion() {
         return fechaPublicacion;
@@ -32,6 +40,11 @@ public class Libro extends Producto implements ILibro{
 
     @Override
     public double getPrecioVenta() {
-        return super.getPrecioVenta();
+        return super.getPrecioVenta()+50;
+    }
+
+    @Override
+    public String toString() {
+        return "publicado el "+ fechaPublicacion.toString() +"por " +autor+ "titulado \""+titulo+"\" y editado por "+editorial+ "al precio de"+ getPrecioVenta() ;
     }
 }

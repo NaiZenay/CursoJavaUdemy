e8a4814940c26d7573652346eb4600f333c3c95f
Ian <115850010+NaiZenay@users.noreply.github.com>
2024-03-31 18:48:24 -0600

    Mostrando detalles de las interfaces

--- src/POO/Interfaces/Imprimible.java
+++ src/POO/Interfaces/Imprimible.java
@@ -6,9 +6,26 @@ Las interfaces detallan el comportamiento de un grupo de objetos
 -todos sus metodos son abtractos (forza el override)
 -todos sus metodos deben ser publicos o default
 -su nombre debe terminar con ible
-
+- todos sus metodos son publicos por default
  */
 public interface Imprimible {
-    public String imprimir();
+    //las constantes tambien son permitidos dentro de las interfaces pero por defecto ya son asi (no se permite otro tipo)
+    final static String x="x";
+
+    String imprimir();
+
+
+    //solo aplicable con los metodos que son default
+    //los metodos default y static son los q tienen permiso de tener su propia implementacion dentro de la interfaz
+    static void metodoOpcional(){
+        System.out.println("Metodo que es parte de la interfaz pero no es obligatorio");
+    }
+
+    default void imprimible(){
+        System.out.println("Metodo propio de la interfaz"+this.getClass().getName());
+    }
+
+
+
 
 }
--- src/POO/Interfaces/ejemplo.java
+++ src/POO/Interfaces/ejemplo.java
@@ -2,26 +2,36 @@ package POO.Interfaces;
 
 public class ejemplo {
     public static void main(String[] args) {
+        //"Instanciando" la interfaz como un clase anonima
+        var imprimir= new Imprimible(){
+            @Override
+            public String imprimir() {
+                return null;
+            }
+        };
         Curriculum cv= new Curriculum("Resumen laboral... ","Alex Flores","Ing. software");
-        cv.addExperiencia("JavaScript");
-        cv.addExperiencia("TypeScript");
-        cv.addExperiencia("CSS");
-        cv.addExperiencia("HTML");
+        cv.addExperiencia("JavaScript")
+        .addExperiencia("TypeScript")
+        .addExperiencia("CSS")
+        .addExperiencia("HTML");
 
         Libro libro= new Libro("Mani",Genero.SUSPENSO,"Libro de Suspenso");
-        libro.addPagina(new Pagina("Capitulo 1"));
-        libro.addPagina(new Pagina("Capitulo 2"));
-        libro.addPagina(new Pagina("Capitulo 3"));
-        libro.addPagina(new Pagina("Capitulo 4"));
-        libro.addPagina(new Pagina("Capitulo 5"));
-        libro.addPagina(new Pagina("Capitulo 6"));
+        libro.addPagina(new Pagina("Capitulo 1"))
+        .addPagina(new Pagina("Capitulo 2"))
+        .addPagina(new Pagina("Capitulo 3"))
+        .addPagina(new Pagina("Capitulo 4"))
+        .addPagina(new Pagina("Capitulo 5"))
+        .addPagina(new Pagina("Capitulo 6"));
 
         Informe informe= new Informe("Estudio sobre microservicios","Ricardo Tapia","BRuno Diaz");
-
 
         imprimir(cv);
         imprimir(libro);
         imprimir(informe);
+
+        Imprimible.metodoOpcional();
+        informe.imprimible();
+
     }
 public static void imprimir(Imprimible imprimible){
     System.out.println(imprimible.imprimir());
